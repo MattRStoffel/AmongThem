58108bef031fa085ef8cc2c39adc282e637d97e9
MattRStoffel <Matt@MrStoffel.com>
2025-03-27 14:41:38 -0700

    Initial Commit

---
+++ AmongThem/AmongThemApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmongThemApp.swift
+//  AmongThem
+//
+//  Created by Matt Stoffel on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct AmongThemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmongThemTests/AmongThemTests.swift
@@ -0,0 +1,17 @@
+//
+//  AmongThemTests.swift
+//  AmongThemTests
+//
+//  Created by Matt Stoffel on 3/27/25.
+//
+
+import Testing
+@testable import AmongThem
+
+struct AmongThemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
